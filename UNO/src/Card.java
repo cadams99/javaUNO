f7c3af1fbafc3e04fb6b68d546fef0555d393e59
Claryse Adams <44442126+cadams99@users.noreply.github.com>
2018-12-20 00:36:53 -0800

    Add files via upload

---
+++ UNO/src/Card.java
@@ -0,0 +1,17 @@
+
+/**
+ * This class will store the properties of a single Uno card.
+ * @author claryse
+ *  
+ */
+ 
+public class Card {
+   private int number; 
+   //TODO: add the other member properties shown in the UML
+   
+   public static void main(String args[]) {
+	   //In Java, each class can have its own main (like int main() from C++)
+   }
+   
+   
+}
